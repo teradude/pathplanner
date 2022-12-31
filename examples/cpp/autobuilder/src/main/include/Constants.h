af86edf4a363fcf2581e3a1db9cdde8a42319b15
Michael Jansen <mjansen4857@gmail.com>
2022-12-31 13:06:59 -0500

    start over with cpp example

--- examples/cpp/autobuilder/src/main/include/Constants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <pathplanner/lib/auto/PIDConstants.h>
-
-namespace swerve{
-    // constexpr pathplanner::PIDConstants AUTO_TRANSLATION_CONSTANTS(0, 0, 0);
-    // constexpr pathplanner::PIDConstants AUTO_ROTATION_CONSTANTS(0, 0, 0);
-}
